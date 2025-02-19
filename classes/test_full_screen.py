5ca28e9714b448e9246e8b9816fb13bff9ccc4af
hammerchu <hammerchu@gmail.com>
2025-02-19 18:47:43 +0800

    debug

--- classes/test_full_screen.py
+++ classes/test_full_screen.py
@@ -1,6 +1,6 @@
 import cv2
 
-im = cv2.imread(cv2.samples.findFile("lena.jpg"))
+im = cv2.imread('./data/text_example.png')
 cv2.namedWindow("foo", cv2.WINDOW_NORMAL)
 cv2.setWindowProperty("foo", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 cv2.imshow("foo", im)
